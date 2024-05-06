32e18126952783e70d06d3aaa9149883898d8c73
opacity-black <2690598117@qq.com>
2024-05-07 00:28:19 +0800

    update eval result

--- eval_tracker/eval_rgbt234.py
+++ eval_tracker/eval_rgbt234.py
@@ -3,6 +3,7 @@ from seqList import where_seq_already
 
 rgbt234 = rgbt.RGBT234()
 result_path = "./tracking_result//caformer/vit_cte_cma_st_f3_lr5e5_ep015/rgbt234"   # 88.5, 66.1
+result_path = "./tracking_result//caformer/vit_cte_cma_st_f3_lr5e5_ep025/rgbt234"   # 87.0, 64.9
 seq_li,l = where_seq_already(result_path)
 print(l)
 
@@ -13,6 +14,12 @@ rgbt234(
     seqs=seq_li
 )
 
+result_path = "./tracking_result//caformer/vit_cte_cma_st_f3_lr5e5_ep015/rgbt234"   # 88.5, 66.1
+rgbt234(
+    tracker_name="T2",
+    result_path=result_path,
+    seqs=seq_li
+)
 
 if __name__=="__main__":
 
@@ -24,7 +31,6 @@ if __name__=="__main__":
 
     msr_dict = rgbt234.MSR(seqs=seq_li)
 
+    print('')
     for k,v in msr_dict.items():
         print(k, "MSR", round(v[0]*100, 1))
-
-    print('')
