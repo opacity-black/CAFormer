2946fd515f0bc7e6b821cbd1507af98bcf8796c8
opacity-black <2690598117@qq.com>
2024-05-14 01:15:59 +0000

    update utils/make_pretrained.py.
    Signed-off-by: opacity-black <2690598117@qq.com>

--- utils/make_pretrained.py
+++ utils/make_pretrained.py
@@ -8,4 +8,18 @@ for k,v in list(stateDict.items()):
     #     stateDict[k.replace('box_head', 'box_head_i')] = stateDict[k]
     if k in ['box_head.conv1_ctr.0.weight','box_head.conv1_offset.0.weight','box_head.conv1_size.0.weight']:
         stateDict[k] = torch.cat([v,v],1)
+
+    if 'qkv' in k:
+        q_key = k.replace('qkv','q_linear')
+        k_key = k.replace('qkv','k_linear')
+        v_key = k.replace('qkv','v_linear')
+        if 'weight' in k:
+            stateDict[q_key] = v[:768, :]
+            stateDict[k_key] = v[768:768*2, :]
+            stateDict[v_key] = v[768*2:, :]
+        elif 'bias' in k:
+            stateDict[q_key] = v[:768]
+            stateDict[k_key] = v[768:768*2]
+            stateDict[v_key] = v[768*2:]
+        del stateDict[k]
 torch.save(stateDict, "./pretrained/CAFormer_SOTPretrained.pth.tar")
